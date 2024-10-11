ae6e50fb22fcba14ad374f60da96cd6de2bda3d4
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2024-10-11 11:52:45 +0100

    euler negative (dy) cfg++ ?

--- scripts/extra_schedulers.py
+++ scripts/extra_schedulers.py
@@ -118,6 +118,31 @@ def sample_euler_cfgpp(model, x, sigmas, extra_args=None, callback=None, disable
         x = denoised + d * sigmas[i+1]
     return x
 
+def sample_euler_negative_cfgpp(model, x, sigmas, extra_args=None, callback=None, disable=None, s_churn=0., s_tmin=0., s_tmax=float('inf'), s_noise=1.):
+    """based on Euler Negative by KoishiStar"""
+    extra_args = {} if extra_args is None else extra_args
+    model.need_last_noise_uncond = True
+    s_in = x.new_ones([x.shape[0]])
+
+    for i in trange(len(sigmas) - 1, disable=disable):
+        gamma = min(s_churn / (len(sigmas) - 1), 2 ** 0.5 - 1) if s_tmin <= sigmas[i] <= s_tmax else 0.
+        eps = torch.randn_like(x) * s_noise
+        sigma_hat = sigmas[i] * (gamma + 1)
+        if gamma > 0:
+            x = x + eps * (sigma_hat ** 2 - sigmas[i] ** 2) ** 0.5
+        denoised = model(x, sigma_hat * s_in, **extra_args)
+        d = model.last_noise_uncond
+
+        if callback is not None:
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigma_hat, 'denoised': denoised})
+
+        # Euler method
+        if sigmas[i + 1] > 0 and i // 2 == 1:
+            x = -denoised - d * sigmas[i+1]
+        else:
+            x = denoised + d * sigmas[i+1]
+    return x
+
 class _Rescaler:
     def __init__(self, model, x, mode, **extra_args):
         self.model = model
@@ -212,11 +237,42 @@ def sample_euler_dy_cfgpp(model, x, sigmas, extra_args=None, callback=None, disa
 
         # Euler method
         x = denoised + d * sigmas[i+1]
+
+        if sigmas[i + 1] > 0:
+            if i // 2 == 1:
+                x = dy_sampling_step_cfgpp(x, model, sigma_hat, **extra_args)        
+
+    return x
+
+@torch.no_grad()
+def sample_euler_negative_dy_cfgpp(model, x, sigmas, extra_args=None, callback=None, disable=None, s_churn=0., s_tmin=0., s_tmax=float('inf'), s_noise=1.):
+    """CFG++ version of Euler Negative Dy by KoishiStar."""
+    extra_args = {} if extra_args is None else extra_args
+    model.need_last_noise_uncond = True
+    s_in = x.new_ones([x.shape[0]])
+
+    for i in trange(len(sigmas) - 1, disable=disable):
+        gamma = min(s_churn / (len(sigmas) - 1), 2 ** 0.5 - 1) if s_tmin <= sigmas[i] <= s_tmax else 0.
+        eps = torch.randn_like(x) * s_noise
+        sigma_hat = sigmas[i] * (gamma + 1)
+        if gamma > 0:
+            x = x + eps * (sigma_hat ** 2 - sigmas[i] ** 2) ** 0.5
+        denoised = model(x, sigma_hat * s_in, **extra_args)
+        d = model.last_noise_uncond
+
+        if callback is not None:
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigma_hat, 'denoised': denoised})
+
+        # Euler method
+        if i // 2 == 1:
+            x = -denoised - d * sigmas[i+1]
+        else:
+            x = denoised + d * sigmas[i+1]
         
         if sigmas[i + 1] > 0:
             if i // 2 == 1:
                 x = dy_sampling_step_cfgpp(x, model, sigma_hat, **extra_args)        
-        
+
     return x
 
 @torch.no_grad()
@@ -321,19 +377,23 @@ try:
         from modules import sd_samplers_common, sd_samplers
         from modules.sd_samplers_kdiffusion import sampler_extra_params, KDiffusionSampler
         samplers_cfgpp = [
-            ("Euler a CFG++",       sample_euler_ancestral_cfgpp, ["k_euler_a_cfgpp"],       {"uses_ensd": True}),
-            ("Euler CFG++",         sample_euler_cfgpp,           ["k_euler_cfgpp"],         {}),
-            ("Euler Dy CFG++",      sample_euler_dy_cfgpp,        ["k_euler_dy_cfgpp"],      {}),
-            ("Euler SMEA Dy CFG++", sample_euler_smea_dy_cfgpp,   ["k_euler_smea_dy_cfgpp"], {}),
+            ("Euler a CFG++",       sample_euler_ancestral_cfgpp,       ["k_euler_a_cfgpp"],       {"uses_ensd": True}),
+            ("Euler CFG++",         sample_euler_cfgpp,                 ["k_euler_cfgpp"],         {}),
+            ("Euler Dy CFG++",      sample_euler_dy_cfgpp,              ["k_euler_dy_cfgpp"],      {}),
+            ("Euler SMEA Dy CFG++", sample_euler_smea_dy_cfgpp,         ["k_euler_smea_dy_cfgpp"], {}),
+            ("Euler Negative CFG++", sample_euler_negative_cfgpp,       ["k_euler_smea_dy_cfgpp"], {}),
+            ("Euler Negative Dy CFG++", sample_euler_negative_dy_cfgpp, ["k_euler_smea_dy_cfgpp"], {}),
         ]
         samplers_data_cfgpp = [
             sd_samplers_common.SamplerData(label, lambda model, funcname=funcname: KDiffusionSampler(funcname, model), aliases, options)
             for label, funcname, aliases, options in samplers_cfgpp
             if callable(funcname)
         ]
-        sampler_extra_params['sample_euler_cfgpp']         = ['s_churn', 's_tmin', 's_tmax', 's_noise']
-        sampler_extra_params['sample_euler_dy_cfgpp']      = ['s_churn', 's_tmin', 's_tmax', 's_noise']
-        sampler_extra_params['sample_euler_smea_dy_cfgpp'] = ['s_churn', 's_tmin', 's_tmax', 's_noise']
+        sampler_extra_params['sample_euler_cfgpp']             = ['s_churn', 's_tmin', 's_tmax', 's_noise']
+        sampler_extra_params['sample_euler_negative_cfgpp']    = ['s_churn', 's_tmin', 's_tmax', 's_noise']
+        sampler_extra_params['sample_euler_dy_cfgpp']          = ['s_churn', 's_tmin', 's_tmax', 's_noise']
+        sampler_extra_params['sample_euler_negative_dy_cfgpp'] = ['s_churn', 's_tmin', 's_tmax', 's_noise']
+        sampler_extra_params['sample_euler_smea_dy_cfgpp']     = ['s_churn', 's_tmin', 's_tmax', 's_noise']
 
         sd_samplers.all_samplers.extend(samplers_data_cfgpp)
         sd_samplers.all_samplers_map = {x.name: x for x in sd_samplers.all_samplers}
